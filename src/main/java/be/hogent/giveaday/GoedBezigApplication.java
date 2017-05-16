71ac11362ca71920fbbbb34b25543a33dde2cc5a
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2017-05-16 11:39:04 +0200

    Initial commit

---
+++ src/main/java/be/hogent/giveaday/GoedBezigApplication.java
@@ -0,0 +1,12 @@
+package be.hogent.giveaday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoedBezigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoedBezigApplication.class, args);
+	}
+}
