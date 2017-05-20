2cc3b77c1309bac8d25eb620b33894a38d609a93
Milan <milandima@gmail.com>
2017-05-20 19:38:00 +0200

    klasse assessment getters en setters + mapping

--- src/main/java/be/hogent/giveaday/model/Assessment.java
+++ src/main/java/be/hogent/giveaday/model/Assessment.java
@@ -1,7 +1,43 @@
 package be.hogent.giveaday.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Milan on 18/05/2017.
  */
+
+@Entity
+@Table(name = "assessment")
 public class Assessment {
+
+    @Id
+    @Column(name = "Group")
+    private String groupName;
+    @Column(name = "SourceUser")
+    private String sourceUser;
+    @Column(name = "TargetUser")
+    private String targetUser;
+    @Column(name = "Assessment")
+    private String assessmentContent;
+
+
+    public String getSourceUser() {
+        return sourceUser;
+    }
+    private void setSourceUser(String sourceUser){ this.sourceUser = sourceUser;}
+
+    public String getTargetUser() {
+        return targetUser;
+    }
+    private void setTargetUser(String targetUser){ this.targetUser = targetUser;}
+
+    public String getAssessmentContent() {
+        return assessmentContent;
+    }
+    private void setAssessmentContent(String assessmentContent){ this.assessmentContent = assessmentContent;}
+
+
 }
