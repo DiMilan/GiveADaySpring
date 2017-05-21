6412ec54851f7174123f88246203ede77ac554c1
Milan Dima <milandima@gmail.com>
2017-05-21 15:18:31 +0200

    Errors uitgehaald eclipselink verwijderd invitation vervolledigd

--- src/main/java/be/hogent/giveaday/model/Invitation.java
+++ src/main/java/be/hogent/giveaday/model/Invitation.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "user_groups")
+@IdClass(InvitationId.class)
 public class Invitation {
 
     @Id
@@ -21,6 +22,8 @@ public class Invitation {
     @PrimaryKeyJoinColumn(name = "GroupId", referencedColumnName = "GroupName")
     private Group group;
 
+    @Column(name = "Status")
+    private InvitationStatus status;
 
     public Invitation() {
     }
@@ -33,5 +36,19 @@ public class Invitation {
         return group;
     }
 
+    public InvitationStatus getStatus() {
+        return status;
+    }
 
+    boolean isPending() {
+        return InvitationStatus.PENDING.equals(status);
+    }
+
+    boolean isAccepted() {
+        return InvitationStatus.ACCEPTED.equals(status);
+    }
+
+    boolean isDeclined() {
+        return InvitationStatus.DECLINED.equals(status);
+    }
 }
