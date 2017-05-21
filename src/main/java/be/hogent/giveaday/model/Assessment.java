cae01fac21122a008fdcd78b4c87ad513dd02f91
Milan <milandima@gmail.com>
2017-05-21 13:19:59 +0200

    datamodel assessmnet aanpassing

--- src/main/java/be/hogent/giveaday/model/Assessment.java
+++ src/main/java/be/hogent/giveaday/model/Assessment.java
@@ -14,14 +14,27 @@ import javax.persistence.Table;
 public class Assessment {
 
     @Id
-    @Column(name = "Group")
+    @Column(name = "GroupNaam")
     private String groupName;
     @Column(name = "SourceUser")
     private String sourceUser;
     @Column(name = "TargetUser")
     private String targetUser;
-    @Column(name = "Assessment")
-    private String assessmentContent;
+    @Column(name = "Comment")
+    private String comment;
+    @Column(name = "Vraag1")
+    private String vraag1;
+    @Column(name = "Vraag2")
+    private String vraag2;
+    @Column(name = "Vraag3")
+    private String vraag3;
+    @Column(name = "Vraag4")
+    private String vraag4;
+    @Column(name = "Vraag5")
+    private String vraag5;
+    @Column(name = "Vraag6")
+    private String vraag6;
+
 
     // KLasse en datamodel nog aan te passen naar model met vragen maar
     // waar vinden we de vragen?
@@ -37,10 +50,23 @@ public class Assessment {
     }
     private void setTargetUser(String targetUser){ this.targetUser = targetUser;}
 
-    public String getAssessmentContent() {
-        return assessmentContent;
+    public String getComment() {
+        return comment;
     }
-    private void setAssessmentContent(String assessmentContent){ this.assessmentContent = assessmentContent;}
+    private void setComment(String assessmentContent){ this.comment = assessmentContent;}
 
+    public String getVraag1() {return vraag1;}
+    private void setVraag1(String vraag1){ this.vraag1 = vraag1;}
+
+    public String getVraag2() {return vraag2;}
+    private void setVraag2(String vraag2){ this.vraag2 = vraag2;}
+    public String getVraag3() {return vraag3;}
+    private void setVraag3(String vraag1){ this.vraag3 = vraag3;}
+    public String getVraag4() {return vraag4;}
+    private void setVraag4(String vraag4){ this.vraag4 = vraag4;}
+    public String getVraag5() {return vraag5;}
+    private void setVraag5(String vraag5){ this.vraag5 = vraag5;}
+    public String getVraag6() {return vraag6;}
+    private void setVraag6(String vraag6){ this.vraag6 = vraag6;}
 
 }
