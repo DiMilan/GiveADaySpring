b7ca69c7ad70f30e5b8378d161ba41c41ab95305
Bart Vanmarcke <bart.vanm@rcke.be>
2017-05-21 09:12:53 +0200

    Add Login View

--- src/main/java/be/hogent/giveaday/config/SecurityConfig.java
+++ src/main/java/be/hogent/giveaday/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.formLogin().usernameParameter("username").passwordParameter("password")
+        http.formLogin().loginPage("/login")
+                .usernameParameter("username").passwordParameter("password")
                 .and()
                 .exceptionHandling().accessDeniedPage("/403");
         http.csrf().disable();
